5970ce4ecc6b2b8d5fc40d16e99012d94e608643
louis_arod <louis.arod@edu.esiee.fr>
2015-01-22 17:45:14 +0100

    Code shorten and improved. Add function Gen.generate(). Improved tests and benchmarks.

--- lxml.go
+++ lxml.go
@@ -1,4 +1,4 @@
-// Package lxml contain tool to generate xml files on the fly.
+// Package lxml contain tools to generate xml files on the fly.
 package lxml
 
 import (
@@ -9,80 +9,85 @@ import (
 
 // Predifined constantes.
 const (
-	XML1     = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-	SVGDoc   = "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"
-	MaxDepth = 127
+	XML1        = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+	SVGDoc      = "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"
+	MaxDepth    = 127
+	MaxNodeName = 255
 )
 
 // Predifined errors
 var (
-	ErrDepthOverflow  = errors.New("lxml.Gen: too deep.")
-	ErrNothingToClose = errors.New("lxml.Gen: nothing to close.")
+	ErrDepthOverflow  = errors.New("lxml.Gen: too deep")
+	ErrNothingToClose = errors.New("lxml.Gen: nothing to close")
+	ErrEmpty          = errors.New("lxml.Gen: empty struct")
 )
 
 // A Gen represente an xml generator.
 type Gen struct {
-	b      bytes.Buffer     // Contain the opened xml
-	opened [MaxDepth]string // Contain the name of the opened elements
-	depth  int8             // Contain the number of opened elements
-	added  bool             // Indicate if the last element has been added or opened
+	xml     bytes.Buffer     // Contains the opened xml
+	closing bytes.Buffer     // Contains the closing elements.
+	opened  [MaxDepth][]byte // Contains the name of the opened elements
+	depth   int8             // Contains the number of opened elements
+	added   bool             // Indicate if the last element has been added or opened
+	changed bool             // Indicate if there has been a change after the generation.
 }
 
-// Create a new Gen with the specified Document type doc.
-func NewGen(version, doctype, root string) *Gen {
-	//g := &Gen{b: bytes.NewBufferString(version + "\n" + doctype + "\n<" +
-	//	root)}
-	g := new(Gen)
-	g.b.WriteString(version + "\n" + doctype + "\n<" + root)
-	g.opened[0] = root
+// NewGen create a new xml.Gen with the specified root element.
+func NewGen(root string) *Gen {
+	g := &Gen{added: false, changed: true}
+	g.xml.WriteString("<" + root)
+	g.opened[0] = []byte(root)
 	g.depth++
 	return g
 }
 
-func (g *Gen) closeLastNode() (err error) {
-	if g.depth <= 0 {
-		return ErrNothingToClose
-	}
-	g.depth--
+// Close the previous element and prepare
+// the new one with correct indentation and
+// opening sign '<'.
+func (g *Gen) closePreviousNode() {
+	// Close the previous node if added
+	buf := make([]byte, MaxDepth*4+5)
+
+	buf[0], buf[1] = '>', '\n'
+	n := 2
 	if g.added {
-		_, err = g.b.WriteString(" /")
+		buf[0], buf[1], buf[2], buf[3] = ' ', '/', '>', '\n'
+		n = 4
 		g.added = false
-	} else {
-		_, err = g.b.WriteString("></" + g.opened[g.depth])
 	}
-	return err
+
+	// Fill the buffer with depth-1 tabs (no tab for the root element)
+	for i, max := 0, int(g.depth); i < max; i++ {
+		//n += copy(buf[n:], "    ")
+		buf[n], buf[n+1], buf[n+2], buf[n+3] = ' ', ' ', ' ', ' '
+		n += 4
+	}
+
+	// Write the opening sign
+	buf[n] = '<'
+	n++
+
+	// Write the node in the buffer
+	g.xml.Write(buf[:n])
+	g.changed = true
 }
 
-// OpenNode open a new node named name with the attributes
-// contains in attr.
+// OpenNode open a new xml node.
 func (g *Gen) OpenNode(name, attr string) (err error) {
 	if g.depth >= MaxDepth {
 		return ErrDepthOverflow
 	}
-	// Close the previous node if added
-	buf := make([]byte, MaxDepth*4+5)
 
-	n := copy(buf, ">\n")
-	if g.added {
-		if g.depth <= 0 {
-			return ErrNothingToClose
-		}
-		n = copy(buf, " />\n")
-		g.added = false
-		g.depth--
-	}
-	// Add to opened nodes
-	g.opened[g.depth] = name
+	g.closePreviousNode()
+
+	// Save the new element
+	//fmt.Printf("OpenNode: depth=%v\n", g.depth)
+	g.opened[g.depth] = []byte(name)
 	g.depth++
 
-	// Fill the buffer with depth-1 tabs (no tab for the root element)
-	for i, max := 0, int(g.depth-1); i < max; i++ {
-		n += copy(buf[n:], "    ")
-	}
-
-	// Write the node in the buffer
-	g.b.Write(buf[:n])
-	g.b.WriteString("<" + name + " " + attr)
+	// Write the new element
+	g.xml.WriteString(name + " " + attr)
+	g.changed = true
 
 	return err
 }
@@ -90,50 +95,42 @@ func (g *Gen) OpenNode(name, attr string) (err error) {
 // AddNode add a closed node to the generator.
 // Convenience function avoiding to call CloseNode for
 // each element without childs.
-func (g *Gen) AddNode(name, attr string) (err error) {
-	err = g.OpenNode(name, attr)
+func (g *Gen) AddNode(name, attr string) {
+	g.closePreviousNode()
+	//fmt.Printf("AddNode: depth=%v\n", g.depth)
+
+	// Write the new element.
+	g.xml.WriteString(name + " " + attr)
+
+	// Keep in mind how the node has been open.
 	g.added = true
-	return err
+	g.changed = true
 }
 
 // AddAttr add an attribute to the last opened node.
 func (g *Gen) AddAttr(a string) {
-	g.b.WriteString(" " + a)
+	g.xml.WriteString(" " + a)
+	g.changed = true
 }
 
 // CloseNode close the last opened node in the generator.
+// This function cannot close the root element, and
+// shoul not be use to close elements added with Gen.AddNode().
 func (g *Gen) CloseNode() error {
-	if g.depth <= 0 {
+	if g.depth <= 1 {
 		return ErrNothingToClose
 	}
 
-	// We allocate a buffer able to contain 4 space for
-	// each opened element + 6 char.
-	buf := make([]byte, MaxDepth*4+6)
-	buf[0], buf[1] = '>', '\n'
-	n := 2
-
-	// We close the previous element if added
-	if g.added {
-		if g.depth <= 1 {
-			return ErrNothingToClose
-		}
-		buf[0], buf[1], buf[2], buf[3] = ' ', '/', '>', '\n'
-		n = 4
-		g.depth--
-		g.added = false
-	}
-
-	// Fill the buffer with depth-1 tabs (no tab for the root element)
-	for i, max := 0, int(g.depth-1); i < max; i++ {
-		n += copy(buf[n:], "    ")
-	}
-	buf[n], buf[n+1] = '<', '/'
-	n += 2
-
-	g.b.Write(buf[:n])
-	g.b.WriteString(g.opened[g.depth-1])
+	// We go back from one level.
 	g.depth--
+
+	g.closePreviousNode()
+
+	// We close the node.
+	g.xml.WriteByte('/')
+	g.xml.Write(g.opened[g.depth])
+	g.changed = true
+
 	return nil
 }
 
@@ -149,57 +146,83 @@ func (g *Gen) CloseNNode(n int) error {
 	return nil
 }
 
-// Read read len(p) byte from the generator.
-func (g *Gen) Read(p []byte) (n int, err error) {
-	if g.b.Len() == 0 {
-		return
-	}
-	// We copy the opened XML
-	n = copy(p, g.b.Bytes())
-	// The slice is already full ? We escape.
-	if l := len(p); l < g.b.Len() {
-		return
-	}
+func (g *Gen) generate() {
+	g.closing.Reset()
 
-	// We create the closing elements
-	// We handle added element
-	i := int(g.depth - 1)
+	// We close the previous element if added.
 	if g.added {
-		p[n], p[n+1] = ' ', '/'
-		n += 2
-		i--
+		//p[n], p[n+1] = ' ', '/'
+		g.closing.Write([]byte(" /"))
 	}
 
 	// We create a string fill with space
 	buf := make([]byte, MaxDepth*4+5)
 	buf[0], buf[1] = '>', '\n'
 	b := 2
-	for j := 0; j < i; j++ {
-		b += copy(buf[b:], "    ")
+	for j, max := 0, int(g.depth-1); j < max; j++ {
+		buf[b] = ' '
+		buf[b+1] = ' '
+		buf[b+2] = ' '
+		buf[b+3] = ' '
+		b += 4
 	}
 	buf[b], buf[b+1] = '<', '/'
 	b += 2
 	// We close all the other element
-	for ; err == nil && i > 0; i-- {
-		n += copy(p[n:], buf[:b])
-		n += copy(p[n:], g.opened[i])
+	for i := int(g.depth - 1); i > 0; i-- {
+		g.closing.Write(buf[:b])
+		g.closing.Write(g.opened[i])
 		// We prepare the next iteration
 		b -= 4
 		buf = buf[:b]
 		buf[b-2], buf[b-1] = '<', '/'
 	}
-	n += copy(p[n:], buf[:b])
-	n += copy(p[n:], g.opened[i])
+	g.closing.Write(buf[:b])
+	g.closing.Write(g.opened[0])
+	g.closing.WriteByte('>')
+	g.changed = false
+}
 
-	if g.depth > 0 {
-		p[n] = '>'
-		n++
-		return n, err
+// Read read len(p) byte from the generator.
+func (g *Gen) Read(p []byte) (n int, err error) {
+	if g.depth == 0 || g.xml.Len() == 0 {
+		return n, ErrEmpty
 	}
+
+	// We copy the opened XML
+	n = copy(p, g.xml.Bytes())
+	if len(p) <= g.xml.Len() {
+		return
+	}
+
+	// If the xml has changed we rewrite the closing elements.
+	if g.changed {
+		g.generate()
+	}
+
+	n += copy(p[n:], g.closing.Bytes())
 	return
 }
 
-// Write the content of the generator until the end of the document.
-func (g *Gen) WriteTo(w io.Writer) (n int64, err error) {
-	return n, err
+// WriteTo write the content of the generator until the end of the document.
+func (g *Gen) WriteTo(w io.Writer) (int64, error) {
+	if g.depth == 0 || g.xml.Len() == 0 {
+		return 0, ErrEmpty
+	}
+
+	// We copy the opened XML
+	n, err := w.Write(g.xml.Bytes())
+	if err != nil {
+		return int64(n), err
+	}
+
+	// We copy the closing tags.
+	var tmp int
+	if g.changed {
+		g.generate()
+	}
+	tmp, err = w.Write(g.closing.Bytes())
+	n += tmp
+
+	return int64(n), err
 }
--- lxml_test.go
+++ lxml_test.go
@@ -3,6 +3,7 @@ package lxml
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
@@ -43,136 +44,86 @@ func max(a, b int) int {
 	return b
 }
 
-var (
-	ReadSvgTest  = XML1 + "\n" + SVGDoc + "\n" + "<svg>\n</svg>"
-	ReadSvgTest2 = XML1 + "\n" + SVGDoc + "\n" +
-		"<svg>\n" +
-		"    <g id='ImUnique'>\n" +
-		"        <rect x='0' y='0' width=\"100%\" height='100%' />\n" +
-		"    </g>\n" +
-		"    <rect x='0' y='0' width=\"100%\" height='100%' />\n" +
-		"    <g class='bench'>\n" +
-		"        <g class='bench'>\n" +
-		"            <rect x='0' y='0' width=\"100%\" height='100%' />\n" +
-		"        </g>\n" +
-		"    </g>\n" +
-		"</svg>"
-)
-
-func TestRead(t *testing.T) {
-	var g Gen
-	// Test empty Gen
-	p := make([]byte, 512)
-	n, _ := g.Read(p)
-	if string(p[:n]) != "" {
-		t.Errorf("Expecting '%s' but got '%s'", "", string(p[:n]))
-	}
-	// Test simple Gen
-	gg := NewGen(XML1, SVGDoc, "svg")
-	n, _ = gg.Read(p)
-	if !bytes.Equal(p[:n], []byte(ReadSvgTest)) {
-		t.Errorf("Expecting '%s' but got '%s'",
-			ReadSvgTest, string(p[:n]))
-	}
-	// Test complex Gen
-	ggg := NewGen(XML1, SVGDoc, "svg")
-	ggg.OpenNode("g", "id='ImUnique'")
-	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
-	ggg.CloseNode()
-	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
-	ggg.OpenNode("g", "class='bench'")
-	ggg.OpenNode("g", "class='bench'")
-	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
-
-	n, _ = ggg.Read(p)
-	assertByte(p, []byte(ReadSvgTest2), t)
-}
-
-var NewGenTest = XML1 + "\n" + SVGDoc + "\n" + "<svg"
+var NewGenTest = "<svg"
 
 func TestNewGen(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	if g == nil {
 		t.Fatal("lxml.NewGen() has return 'nil'.")
 	}
-	if !bytes.Equal(g.b.Bytes(), []byte(NewGenTest)) {
+	if !bytes.Equal(g.xml.Bytes(), []byte(NewGenTest)) {
 		t.Errorf("Expecting '%s' but got '%s'",
-			NewGenTest, string(g.b.Bytes()))
+			NewGenTest, string(g.xml.Bytes()))
 	}
 	if g.depth != 1 {
 		t.Fatal("Expecting a depth of 1 but got %d", g.depth)
 	}
-	if g.opened[g.depth-1] != "svg" {
+	if string(g.opened[g.depth-1]) != "svg" {
 		t.Fatal("Expecting 'svg' in g.opened bug got %s", g.opened[g.depth-1])
 
 	}
 }
 
-var AddNodeTest = XML1 + "\n" + SVGDoc + "\n" +
-	"<svg>\n" +
-	"    <rect width=\"100%\" height='100%' />\n" +
-	"    <rect width=\"100%\" height='100%'"
+var AddNodeTest = `<svg>
+    <rect width="100%" height='100%' />
+    <rect width="100%" height='100%'`
 
 func TestAddNode(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	g.AddNode("rect", "width=\"100%\" height='100%'")
 	g.AddNode("rect", "width=\"100%\" height='100%'")
-	assertByte(g.b.Bytes(), []byte(AddNodeTest), t)
+	assertByte(g.xml.Bytes(), []byte(AddNodeTest), t)
 }
 
-var AddAttrTest = XML1 + "\n" + SVGDoc + "\n" +
-	"<svg>\n" +
-	"    <rect width=\"100%\" height='100%' style='fill: blue' id='myRect'"
+var AddAttrTest = `<svg>
+    <rect width="100%" height='100%' style='fill: blue' id='myRect'`
 
 func TestAddAttr(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	g.AddNode("rect", "width=\"100%\" height='100%'")
 	g.AddAttr("style='fill: blue'")
 	g.AddAttr("id='myRect'")
-	assertByte(g.b.Bytes(), []byte(AddAttrTest), t)
+	assertByte(g.xml.Bytes(), []byte(AddAttrTest), t)
 }
 
-var OpenNodeTest = XML1 + "\n" + SVGDoc + "\n" +
-	"<svg>\n" +
-	"    <rect width=\"100%\" height='100%'>\n" +
-	"        <rect width=\"100%\" height='100%'"
+var OpenNodeTest = `<svg>
+    <rect width="100%" height='100%'>
+        <rect width="100%" height='100%'`
 
 func TestOpenNode(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	g.OpenNode("rect", "width=\"100%\" height='100%'")
 	g.OpenNode("rect", "width=\"100%\" height='100%'")
-	assertByte(g.b.Bytes(), []byte(OpenNodeTest), t)
+	assertByte(g.xml.Bytes(), []byte(OpenNodeTest), t)
 }
 
-var CloseNodeTest = XML1 + "\n" + SVGDoc + "\n" +
-	"<svg>\n" +
-	"    <rect width=\"100%\" height='100%'>\n" +
-	"        <rect width=\"100%\" height='100%' />\n" +
-	"    </rect>\n" +
-	"    <rect width=\"100%\" height='100%'"
+var CloseNodeTest = `<svg>
+    <rect width="100%" height='100%'>
+        <rect width="100%" height='100%' />
+    </rect>
+    <rect width="100%" height='100%'`
 
 func TestCloseNode(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	g.OpenNode("rect", "width=\"100%\" height='100%'")
 	g.AddNode("rect", "width=\"100%\" height='100%'")
 	g.CloseNode()
 	g.OpenNode("rect", "width=\"100%\" height='100%'")
-	assertByte(g.b.Bytes(), []byte(CloseNodeTest), t)
+	assertByte(g.xml.Bytes(), []byte(CloseNodeTest), t)
 }
 
-var CloseNNodeTest = XML1 + "\n" + SVGDoc + "\n" +
-	"<svg>\n" +
-	"    <g class='bench'>\n" +
-	"        <g class='bench'>\n" +
-	"            <g class='bench'>\n" +
-	"                <rect x='0' y='0' width=\"100%\" height='100%' />\n" +
-	"            </g>\n" +
-	"        </g>\n" +
-	"    </g>\n" +
-	"    <rect x='0' y='0' width=\"100%\" height='100%'"
+var CloseNNodeTest = `<svg>
+    <g class='bench'>
+        <g class='bench'>
+            <g class='bench'>
+                <rect x='0' y='0' width="100%" height='100%' />
+            </g>
+        </g>
+    </g>
+    <rect x='0' y='0' width="100%" height='100%'`
 
 func TestCloseNNode(t *testing.T) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	n := 3
 	for i := 0; i < n; i++ {
 		g.OpenNode("g", "class='bench'")
@@ -188,15 +139,153 @@ func TestCloseNNode(t *testing.T) {
 		t.Fatalf("Fail to close %d nodes with error: %v", n, err)
 	}
 	g.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
-	assertByte(g.b.Bytes(), []byte(CloseNNodeTest), t)
+	assertByte(g.xml.Bytes(), []byte(CloseNNodeTest), t)
+}
+
+func TestGenerate(t *testing.T) {
+	g := NewGen("svg")
+	g.generate()
+	t.Log("Gen.closing:", string(g.closing.Bytes()))
+}
+
+var (
+	ReadSvgTest  = "<svg>\n</svg>"
+	ReadSvgTest2 = `<svg>
+    <g id='ImUnique'>
+        <rect x='0' y='0' width="100%" height='100%' />
+    </g>
+    <rect x='0' y='0' width="100%" height='100%' />
+    <g class='bench'>
+        <g class='bench'>
+            <rect x='0' y='0' width="100%" height='100%' />
+        </g>
+    </g>
+</svg>`
+)
+
+func TestRead(t *testing.T) {
+	var g Gen
+	// Test empty Gen
+	p := make([]byte, 512)
+	n, _ := g.Read(p)
+	if string(p[:n]) != "" {
+		t.Errorf("Expecting '%s' but got '%s'", "", string(p[:n]))
+	}
+	// Test simple Gen
+	gg := NewGen("svg")
+	n, _ = gg.Read(p)
+	if !bytes.Equal(p[:n], []byte(ReadSvgTest)) {
+		t.Errorf("Expecting '%s' but got '%s'",
+			ReadSvgTest, string(p[:n]))
+	}
+	// Test complex Gen
+	ggg := NewGen("svg")
+	ggg.OpenNode("g", "id='ImUnique'")
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+	ggg.CloseNode()
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+	ggg.OpenNode("g", "class='bench'")
+	ggg.OpenNode("g", "class='bench'")
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+
+	n, _ = ggg.Read(p)
+	assertByte(p, []byte(ReadSvgTest2), t)
 }
 
 func TestWriteTo(t *testing.T) {
+	buf := &bytes.Buffer{}
+	buf.Grow(2048)
+
+	assertErr := func(err error) {
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	assertLen := func(n, dn int64) {
+		if n != dn {
+			t.Errorf("Expecting length of '%d' but got '%d'", dn, n)
+		}
+	}
+
+	// Test empty Gen
+	var g Gen
+	n, err := g.WriteTo(buf)
+	if err != ErrEmpty {
+		t.Errorf("Expecting error '%v' but got '%v'", ErrEmpty, err)
+	}
+	buf.Reset()
+
+	// Test simple Gen
+	gg := NewGen("svg")
+	n, err = gg.WriteTo(buf)
+	assertErr(err)
+	assertLen(n, int64(len(ReadSvgTest)))
+	if !bytes.Equal(buf.Bytes(), []byte(ReadSvgTest)) {
+		t.Errorf("Expecting '%s' but got '%s'",
+			ReadSvgTest, string(buf.Bytes()))
+	}
+	buf.Reset()
+
+	// Test complex Gen
+	ggg := NewGen("svg")
+	ggg.OpenNode("g", "id='ImUnique'")
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+	ggg.CloseNode()
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+	ggg.OpenNode("g", "class='bench'")
+	ggg.OpenNode("g", "class='bench'")
+	ggg.AddNode("rect", "x='0' y='0' width=\"100%\" height='100%'")
+
+	n, err = ggg.WriteTo(buf)
+	assertErr(err)
+	assertLen(n, int64(len(ReadSvgTest2)))
+	assertByte(buf.Bytes(), []byte(ReadSvgTest2), t)
 
 }
 
+/*func BenchmarkEq(b *testing.B) {
+	buf := make([]byte, 2048)
+	n, m := 0, 0
+	for i := 0; i < b.N; i++ {
+		buf[m], buf[m+1], buf[m+2] = ':', ')', ' '
+		n += 3
+		m = n % (2048 - 3)
+	}
+}
+
+func BenchmarkCopy(b *testing.B) {
+	buf := make([]byte, 2048)
+	n, m := 0, 0
+	for i := 0; i < b.N; i++ {
+		n += copy(buf[m:], ":) ")
+		m = n % (2048 - 3)
+	}
+}*/
+
+func BenchmarkAddNode(b *testing.B) {
+	g := NewGen("svg")
+	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
+	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		g.AddNode("rect", "x='10' y='10' width=\"50\" height='50'")
+	}
+}
+
+func BenchmarkAddAttr(b *testing.B) {
+	g := NewGen("svg")
+	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
+	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		g.AddAttr("style='fill: rgb(2,3,4);'")
+	}
+}
+
 func BenchmarkRead(b *testing.B) {
-	g := NewGen(XML1, SVGDoc, "svg")
+	g := NewGen("svg")
 	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
 	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
 
@@ -223,35 +312,52 @@ func BenchmarkRead(b *testing.B) {
 	//ioutil.WriteFile("benchmarkRead.svg", buf[:n], 0755)
 }
 
-func BenchmarkAddNode(b *testing.B) {
-	g := NewGen(XML1, SVGDoc, "svg")
+func BenchmarkWriteTo(b *testing.B) {
+	g := NewGen("svg")
 	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
 	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
 
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		g.AddNode("rect", "x='10' y='10' width=\"50\" height='50'")
+	for i := 0; i < 15; i++ {
+		g.OpenNode("g", "")
+		g.AddNode("rect", fmt.Sprintf("x='%d' y='%d' width=\"%d\" height='%d'", i*10, i*10, 400-i*20, 400-i*20))
+		g.AddAttr(fmt.Sprintf("style='fill: rgb(%d,%d,%d);'", i*10, 0, i*20))
 	}
+
+	var n int64
+	var err error
+	buf := new(bytes.Buffer)
+	buf.Grow(32768)
+	//buf := make([]byte, 32768)
+	//b.Logf("g.depth: %d, g.b.Len(): %d", g.depth, g.b.Len())
+	b.ReportAllocs()
+	for i := 0; err == nil && i < b.N; i++ {
+		//b.Logf("g.depth: %d, g.b.Len(): %d", g.depth, g.b.Len())
+		buf.Reset()
+		n, err = g.WriteTo(buf)
+		b.SetBytes(n)
+	}
+	if err != nil {
+		b.Fatal("Gen.WriteTo() has return the error:", err)
+	}
+	if n <= 0 {
+		b.Fatal("Gen.WriteTo() expected to write more than 0 bytes and wrote :", n)
+	}
+	ioutil.WriteFile("benchmarkRead.svg", buf.Bytes(), 0755)
 }
 
-func BenchmarkAddAttr(b *testing.B) {
-	g := NewGen(XML1, SVGDoc, "svg")
+func BenchmarkGenerate(b *testing.B) {
+	g := NewGen("svg")
 	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
 	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
 
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		g.AddAttr("style='fill: rgb(2,3,4);'")
+	for i := 0; i < 15; i++ {
+		g.OpenNode("g", "")
+		g.AddNode("rect", fmt.Sprintf("x='%d' y='%d' width=\"%d\" height='%d'", i*10, i*10, 400-i*20, 400-i*20))
+		g.AddAttr(fmt.Sprintf("style='fill: rgb(%d,%d,%d);'", i*10, 0, i*20))
 	}
-}
-
-func BenchmarkOpenNode(b *testing.B) {
-	g := NewGen(XML1, SVGDoc, "svg")
-	g.AddAttr("width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'")
-	g.AddNode("rect", "x='0' y='0' width=\"400\" height='400' style='fill: white; stroke: black; stroke-width: 1px;'")
 
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		g.OpenNode("g", "class='benchmark'")
+		g.generate()
 	}
 }
